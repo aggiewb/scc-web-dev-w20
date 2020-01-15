46466a733418d96873f9f30f249c1813d776de50
AggieWB <usmaggie23@gmail.com>
2020-01-14 21:45:13 -0800

    Add assignment 2 for itc 115

---
+++ itc115/itc115-assigment2/src/Fibonacci_Numbers.java
@@ -0,0 +1,16 @@
+
+public class Fibonacci_Numbers {
+	public static void main(String[] args) {
+		int oneNumAgo = 1;
+		int twoNumAgo = 1;
+		int currentNum = oneNumAgo + twoNumAgo;
+		System.out.print(oneNumAgo + " " + twoNumAgo + " " + currentNum + " ");
+
+		for (int i = 1; i < 10; i++) {
+		    oneNumAgo = twoNumAgo;
+		    twoNumAgo = currentNum;
+		    currentNum = oneNumAgo + twoNumAgo;
+		    System.out.print(currentNum + " ");
+		}
+	}
+}
