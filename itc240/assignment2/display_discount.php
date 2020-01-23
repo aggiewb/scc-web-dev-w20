116e9c543900111e7ad7eff5d74550b0f6d5e789
AggieWB <usmaggie23@gmail.com>
2020-01-23 07:52:21 -0800

    Add main tag to body

--- itc240/assignment2/display_discount.php
+++ itc240/assignment2/display_discount.php
@@ -22,20 +22,22 @@ $discount_price_f = "$" . number_format($discount_price, 2);
         <link rel="stylesheet" type="text/css" href="main.css">
     </head>
     <body>
-        <h1>Product Discount Calculator</h1>
-        <label>Product Description</label>
-        <span><?php echo htmlspecialchars($product_description); ?></span>
-        <br>
-        <label>List Price</label>
-        <span><?php echo htmlspecialchars($list_price_f); ?></span>
-        <br>
-        <label>Standard Discount</label>
-        <span><?php echo htmlspecialchars($discount_percent_f); ?></span>
-        <br>
-        <label>Discount Amount</label>
-        <span><?php echo htmlspecialchars($discount_f); ?></span>
-        <br>
-        <label>Discount Price</label>
-        <span><?php echo $discount_price; ?></span>
+        <main>
+            <h1>Product Discount Calculator</h1>
+            <label>Product Description</label>
+            <span><?php echo htmlspecialchars($product_description); ?></span>
+            <br>
+            <label>List Price</label>
+            <span><?php echo htmlspecialchars($list_price_f); ?></span>
+            <br>
+            <label>Standard Discount</label>
+            <span><?php echo htmlspecialchars($discount_percent_f); ?></span>
+            <br>
+            <label>Discount Amount</label>
+            <span><?php echo htmlspecialchars($discount_f); ?></span>
+            <br>
+            <label>Discount Price</label>
+            <span><?php echo $discount_price; ?></span>
+        </main> 
     </body>
 </html>
