576b6ddbc12a7fda1e56bf195df77abf6f10c056
AggieWB <usmaggie23@gmail.com>
2020-01-30 13:09:24 -0800

    Add comments for assigned variables, and add variables for formatted dollar amount

--- itc240/assignment4/conditions_and_loops.php
+++ itc240/assignment4/conditions_and_loops.php
@@ -1,11 +1,17 @@
 <?php
-
+//total meal cost section variables
 $meal_cost = 41.55;
 $tip_percent = 20 / 100;
 $tip_amount = $meal_cost * $tip_percent;
 $tax_percent = 10.1 / 100;
 $tax_amount = $meal_cost * $tax_percent;
 $total_cost = $meal_cost + $tax_amount + $tip_amount;
+
+//meal cost section formatted variables
+$meal_cost_f = number_format($meal_cost, 2);
+$tip_amount_f = number_format($tip_amount, 2);
+$tax_amount_f = number_format($tax_amount, 2);
+$total_cost_f = number_format($total_cost, 2);
 
 
 ?>
