1bc34eee263f2bb55912429d53514cd1305ab74d
AggieWB <usmaggie23@gmail.com>
2020-01-30 13:03:13 -0800

    Add variables and operations for total meal cost section

---
+++ itc240/assignment4/conditions_and_loops.php
@@ -0,0 +1,11 @@
+<?php
+
+$meal_cost = 41.55;
+$tip_percent = 20 / 100;
+$tip_amount = $meal_cost * $tip_percent;
+$tax_percent = 10.1 / 100;
+$tax_amount = $meal_cost * $tax_percent;
+$total_cost = $meal_cost + $tax_amount + $tip_amount;
+
+
+?>
