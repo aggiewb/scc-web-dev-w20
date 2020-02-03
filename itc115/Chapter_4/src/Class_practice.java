7d6681581a85e8b00a04218ecf715855a078fe08
Aggie Wheeler Bateman <48076903+aggiewb@users.noreply.github.com>
2020-02-03 13:43:26 -0800

    Add new class for practice, and add method to practice fence post algorithum

---
+++ itc115/Chapter_4/src/Class_practice.java
@@ -0,0 +1,19 @@
+
+public class Class_practice {
+
+	public static void main(String[] args) {
+		printNumbers(9);
+
+	}
+	
+	public static void printNumbers(int maxNum) {
+		for(int i = 1; i < maxNum; i++) {
+			System.out.print(i + ", ");
+		}
+		
+		System.out.print(maxNum);
+	}
+	
+	
+
+}
