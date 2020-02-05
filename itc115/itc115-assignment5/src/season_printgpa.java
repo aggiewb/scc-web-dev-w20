6c719af9bb6bc89fed4477e6b9857e5aa130d609
AggieWB <usmaggie23@gmail.com>
2020-02-05 10:02:31 -0800

    Add test calls for season method

--- itc115/itc115-assignment5/src/season_printgpa.java
+++ itc115/itc115-assignment5/src/season_printgpa.java
@@ -2,8 +2,19 @@
 public class season_printgpa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		season(1, 21);
+		season(2, 28);
+		season(3, 15);
+		season(3, 16);
+		season(4, 1);
+		season(6, 15);
+		season(6, 16);
+		season(8, 9);
+		season(9, 15);
+		season(9, 16);
+		season(10, 31);
+		season(12, 15);
+		season(12, 16);
 	}
 	
 	public static String season(int month, int day){
