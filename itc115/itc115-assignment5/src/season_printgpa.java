58586ce633936e0788efb6603b4d920e8ff3cb1e
AggieWB <usmaggie23@gmail.com>
2020-02-06 09:08:53 -0800

    Add method for printGPA assignment; import library for use of console; call printGPA method in main

--- itc115/itc115-assignment5/src/season_printgpa.java
+++ itc115/itc115-assignment5/src/season_printgpa.java
@@ -1,7 +1,11 @@
 
+import java.util.*;
+
 public class season_printgpa {
 
 	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		printGPA(console);
 		season(1, 21);
 		season(2, 28);
 		season(3, 15);
@@ -31,6 +35,55 @@ public class season_printgpa {
 	        System.out.println("Fall");
 	    	return "Fall";
 	    }
+	}
+	
+	public static void printGPA(Scanner console){
+	    System.out.print("Enter a student record: ");
+	    String studentRecord = console.nextLine();
+	    console.close();
+	   
+	    int spaceBetween = studentRecord.indexOf(" ");
+	    String studentName = "";
+	    
+	    for(int i = 0; i < spaceBetween; i++){
+	          studentName = studentName + studentRecord.charAt(i);
+	    }
+	    
+	    int numOfScores = Character.getNumericValue(studentRecord.charAt(studentName.length() + 1));
+	    
+	    String num = "";
+	    int sum = 0;
+	    for(int i = studentName.length() + 3; i < studentRecord.length(); i++){
+	       if(Character.isDigit(studentRecord.charAt(i))){
+	           num = num + studentRecord.charAt(i);
+	       } else {
+	           if (num.length() == 1){
+	               sum += Character.getNumericValue(num.charAt(0));
+	           } else if (num.length() == 2){
+	                sum += Character.getNumericValue(num.charAt(0)) * 10;
+	                sum += Character.getNumericValue(num.charAt(1));
+	           } else if (num.length() == 3){
+	               sum += Character.getNumericValue(num.charAt(0)) * 100;
+	               sum += Character.getNumericValue(num.charAt(1)) * 10;
+	               sum += Character.getNumericValue(num.charAt(2));
+	           }
+	           num = "";
+	       }
+	    }
+	    
+	    if (num.length() == 1){
+	        sum += Character.getNumericValue(num.charAt(0));
+	    }else if(num.length() == 2){
+	       sum += Character.getNumericValue(num.charAt(0)) * 10;
+	       sum += Character.getNumericValue(num.charAt(1));
+	    } else if (num.length() == 3){
+	        sum += Character.getNumericValue(num.charAt(0)) * 100;
+	        sum += Character.getNumericValue(num.charAt(1)) * 10;
+	        sum += Character.getNumericValue(num.charAt(2));
+	    }
+	       
+	    double studentGPA = (double)sum / numOfScores;
+	    System.out.println(studentName + "\'s grade is " + studentGPA);
 	}
 
 }
