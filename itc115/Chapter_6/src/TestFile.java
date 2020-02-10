d4081ae3b9631fdc554e7dcfc466cf0f44832ed3
Aggie Wheeler Bateman <48076903+aggiewb@users.noreply.github.com>
2020-02-10 14:33:29 -0800

    New class to demonstrate Java file manipulation

---
+++ itc115/Chapter_6/src/TestFile.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+public class TestFile {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		PrintStream output = new PrintStream(new File("hello.txt"));
+
+		output.println("Hello world");
+		output.println();
+		output.println("This program produces");
+		output.println("four lines of outupt");
+		output.println("the old content is gone");
+		
+		output.close();
+	
+
+	}
+	
+	public static void testFile1() {
+		File f = new File("hamlet.txt");
+		System.out.println("Exists returns: " + f.exists());
+		System.out.println("Can read returns: " + f.canRead());
+		System.out.println("Length returns: " + f.length());
+		System.out.println("Absolute path returns: " + f.getAbsolutePath());
+	}
+	
+	
+
+}
