7fc4892fd7285ee80c014e4490e211411af1f1d4
Aggie Wheeler Bateman <48076903+aggiewb@users.noreply.github.com>
2020-02-10 15:33:01 -0800

    Add class to reverse array

--- itc115/Chapter_6/src/TestArrays.java
+++ itc115/Chapter_6/src/TestArrays.java
@@ -23,6 +23,20 @@ public class TestArrays {
 		System.out.println();
 		int[] testData = {1, 2, 3, 4, 5};
 		System.out.println(Arrays.toString(testData));
+		
+		double [][] temp = new double [3][5];
+		
+		temp[0][0] = 98.3;
+		temp[0][1] = 85.1;
+		temp[0][2] = 77.4;
+		temp[1][0] = 70.5;
+		temp[1][1] = 88.9;
+		temp[1][2] = 88.9;
+		temp[2][0] = 88.9;
+		temp[2][1] = 88.9;
+		temp[2][2] = 88.9;
+		
+		System.out.println(temp[2][0]);
 
 	}
 
