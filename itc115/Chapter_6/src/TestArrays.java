81f401bf5c3e42797ee71a5cbf081cbd7b07d010
Aggie Wheeler Bateman <48076903+aggiewb@users.noreply.github.com>
2020-02-10 15:02:04 -0800

    Add class to demonstrate arrays

---
+++ itc115/Chapter_6/src/TestArrays.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class TestArrays {
+
+	public static void main(String[] args) {
+		int[] numbers = new int[10];
+		
+		numbers[0] = 42;
+		numbers[5] = 7;
+		
+		System.out.println(numbers[0]);
+		System.out.println(numbers[1]);
+		
+		System.out.println(Arrays.toString(numbers));
+		
+		for(int i = 0; i < numbers.length; i++) {
+			numbers[i] = i * i;
+			System.out.print(numbers[i] + " ");
+		}
+
+	}
+
+}
