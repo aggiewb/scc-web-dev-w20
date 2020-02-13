381cdac42673c2737de0e4203e1846e2f70e8c28
AggieWB <usmaggie23@gmail.com>
2020-02-13 12:18:10 -0800

    Add Scanner to read txt file, and a call to the method boyGirl

--- itc115/itc115-assignment6/src/BoysGirls.java
+++ itc115/itc115-assignment6/src/BoysGirls.java
@@ -3,7 +3,8 @@ import java.util.*;
 
 public class BoysGirls {
 	public static void main(String[] args) throws FileNotFoundException {
-
+		Scanner input = new Scanner(new File("boysgirls.txt"));
+		boyGirl(input);
 	}
 	
 	public static void boyGirl() throws FileNotFoundException{
