aa628ce64921c08f5b4b8757d6d8b7e004392603
AggieWB <usmaggie23@gmail.com>
2020-02-13 12:43:46 -0800

    Add logic for girlGirl method

--- itc115/itc115-assignment6/src/BoysGirls.java
+++ itc115/itc115-assignment6/src/BoysGirls.java
@@ -7,8 +7,31 @@ public class BoysGirls {
 		boyGirl(input);
 	}
 	
-	public static void boyGirl() throws FileNotFoundException{
+	public static void boyGirl(Scanner console) throws FileNotFoundException{
+		int boys = 0;
+		int girls = 0;
+		int count = 0;
+		int difference = 0;
+		    
+		while(console.hasNext()){
+			count++;
+			
+			if(count % 2 == 0){
+				girls++;
+				} else if (count % 2 == 0){
+					boys++;
+					}
+			console.next();
+			}
 		
+		if(boys > girls){
+			difference = boys - girls;
+			} else {
+				difference = girls - boys;
+				}
+		
+		System.out.println(boys + " boys, " + girls + " girls");
+		System.out.println("Difference between boys' and girls' sums: " + difference);
+		}	
 	}
 
-}
