19c8167a0b3773e3f97c9fc029048567da17ea92
AggieWB <usmaggie23@gmail.com>
2020-02-13 12:15:30 -0800

    Add Java project, class, and basic class structure

---
+++ itc115/itc115-assignment6/src/BoysGirls.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class BoysGirls {
+	public static void main(String[] args) throws FileNotFoundException {
+
+	}
+	
+	public static void boyGirl() throws FileNotFoundException{
+		
+	}
+
+}
