b2e146beaee116411fb6dc11c118b133ca51050e
AggieWB <usmaggie23@gmail.com>
2020-02-14 12:03:53 -0800

    Sort $statecaps and display each state and capital in a ul tag list
    Remove p tag from Part 2 section

--- itc240/assignment6_arrays.php
+++ itc240/assignment6_arrays.php
@@ -10,6 +10,7 @@ $bird = $pets[3]
 /*Display the state and state capital from the below array $statecaps. Sort the list by the name of the capital. $statecaps = array("Washington => Olympia", "Oregon => Salem", "California => Sacramento", "Alaska => Juneau", "Hawaii => Honolulu", "Idaho => Boise", "Nevada => Carson City", "Arizona => Phoenix"); */
 
 $statecaps = array("Washington => Olympia", "Oregon => Salem", "California => Sacramento", "Alaska => Juneau", "Hawaii => Honolulu", "Idaho => Boise", "Nevada => Carson City", "Arizona => Phoenix");
+asort($statecaps);
 
 /*Insert a new item in an array with elements 1, 2, 3, 4, 5, 6
 Result: The array should now contain 1, 2, 3, 4, 42, 5, 6*/
@@ -33,7 +34,13 @@ $weekTemperatures = array(32, 47, 55, 30, 42, 39, 45);
         <p>You can adopt a <?php echo $dog;?>, a <?php echo $cat;?>, or a <?php echo $bird?> from the animal sanctuary.</p>
         
         <h2>Part 2</h2>
-        <p></p>
+        <?php 
+        echo'<ul>';
+        foreach ($statecaps as $state => $capital){
+            echo '<li>$state, $capital</li>';
+        }
+        echo '</ul>';
+        ?>
 
         <h2>Part 3</h2>
         <p></p>
