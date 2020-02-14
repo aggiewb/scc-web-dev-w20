07dec747bda98db799f59fdee43e89e70618aaba
AggieWB <usmaggie23@gmail.com>
2020-02-14 11:29:41 -0800

    Add arrays needed for assignment 6

--- itc240/assignment6_arrays.php
+++ itc240/assignment6_arrays.php
@@ -2,11 +2,19 @@
 /*Given the following of pets containing the items ('dog', 'cat', 'fish', 'bird', 'iguana'); display the following text, pulling the animal types from the pets array.
 Result: "You can adopt a dog, a cat, or a bird from the animal sanctuary." */
 
+$pets = array('dog', 'cat', 'fish', 'bird', 'iguana');
+
 /*Display the state and state capital from the below array $statecaps. Sort the list by the name of the capital. $statecaps = array("Washington => Olympia", "Oregon => Salem", "California => Sacramento", "Alaska => Juneau", "Hawaii => Honolulu", "Idaho => Boise", "Nevada => Carson City", "Arizona => Phoenix"); */
+
+$statecaps = array("Washington => Olympia", "Oregon => Salem", "California => Sacramento", "Alaska => Juneau", "Hawaii => Honolulu", "Idaho => Boise", "Nevada => Carson City", "Arizona => Phoenix");
 
 /*Insert a new item in an array with elements 1, 2, 3, 4, 5, 6
 Result: The array should now contain 1, 2, 3, 4, 42, 5, 6*/
 
+$numbers = array(1, 2, 3, 4, 5, 6);
+
 /*Given an array of week’s temperatures containing the items 32, 47, 55, 30, 42, 39, 45, calculate and display the average temperature as well as display the lowest and highest temperature in the week.
 Result Average temp this week : 41.4 Lowest temp: 30 Highest temp : 55 */
+
+$weekTemperatures = array(32, 47, 55, 30, 42, 39, 45);
 ?>
