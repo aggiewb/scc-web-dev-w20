d219f0f1c5226a4d78ccb90bd396e36b314f0e30
AggieWB <usmaggie23@gmail.com>
2020-02-21 12:41:39 -0800

    Add function for sorting an array

--- itc240/assignment7_functions.php
+++ itc240/assignment7_functions.php
@@ -13,6 +13,18 @@ function isPalindrome($str){
     }
 }
 
+function sortArray($arr){
+    for($i = 0; $i < count($arr); $i++){
+        for($j = $i + 1; $j < count($arr); $j++){
+            if($arr[$i] > $arr[$j]){
+                $temp = $arr[$i];
+                $arr[$i] = $arr[$j];
+                $arr[$j] = $temp;
+            }     
+        }
+    }
+}
+
 ?>
 <html>
     <head>
