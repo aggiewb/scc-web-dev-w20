cf481e7d23411310d957a8fe320a7fc6eeac92e4
AggieWB <usmaggie23@gmail.com>
2020-02-21 09:02:23 -0800

    Add code to get word from user and store the store in a php variable

--- itc240/assignment7_functions.php
+++ itc240/assignment7_functions.php
@@ -1,7 +1,15 @@
 <?php
 
+$check = $_POST['check'];
+
 ?>
 <html>
     <head></head>
-    <body></body>
+    <body>
+        <form method="post">
+            <label for="check">Enter word or phrase:</label>
+            <input type="text" id="word" name="check">
+            <input type="submit" value="Submit">
+        </form>
+    </body>
 </html>
