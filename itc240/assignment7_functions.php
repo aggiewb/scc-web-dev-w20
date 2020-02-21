c04fd54b10941d7fc00edbf510cdf40602a462b3
AggieWB <usmaggie23@gmail.com>
2020-02-21 09:16:44 -0800

    Add function for checking if user input is a palindrome
    Add name attribute for form input tag
    Change user input name attribute name

--- itc240/assignment7_functions.php
+++ itc240/assignment7_functions.php
@@ -1,14 +1,23 @@
 <?php
 
-$check = $_POST['check'];
+$userWordPhrase = $_POST['userWordPhrase'];
+
+function isPalindrome($str){
+    $backwardsWordPhrase = strrev($str);
+    $palindrome = strcmp($userWordPhrase, $backwardsWordPhrase);
+    if($palindrome == 0){
+        return true;
+    }
+}
+
 
 ?>
 <html>
     <head></head>
     <body>
         <form method="post">
-            <label for="check">Enter word or phrase:</label>
-            <input type="text" id="word" name="check">
+            <label for="userWordPhrase">Enter word or phrase:</label>
+            <input type="text" id="userWordPhrase" name="userWordPhrase">
             <input type="submit" value="Submit">
         </form>
     </body>
