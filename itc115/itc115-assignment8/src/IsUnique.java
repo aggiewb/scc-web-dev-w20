1160cdd2f216bd8d8c270ce461336a6f2dd3e964
AggieWB <usmaggie23@gmail.com>
2020-02-25 12:41:17 -0800

    Remove library import not needed

--- itc115/itc115-assignment8/src/IsUnique.java
+++ itc115/itc115-assignment8/src/IsUnique.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 /*This class contains a method named isUnique that takes an array of integers as a parameter and that returns a boolean 
  * value indicating whether or not the values in the array are unique (true for yes, false for no). 
  * The values in the list are considered unique if there is no pair of values that are equal. 
