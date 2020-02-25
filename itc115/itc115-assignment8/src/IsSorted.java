5fd67c77b4e1abb8314582de6ef69544bafe1ade
AggieWB <usmaggie23@gmail.com>
2020-02-25 11:06:20 -0800

    Add method the contains algorithm to sort and compare an array
    Import java library Java.util

--- itc115/itc115-assignment8/src/IsSorted.java
+++ itc115/itc115-assignment8/src/IsSorted.java
@@ -1,9 +1,33 @@
 
+import java.util.*;
+
 public class IsSorted {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public static boolean isSorted(double[] num){
+	    boolean isSorted = false;
+	    
+	    double[] newNum = Arrays.copyOf(num, num.length);
+	    
+	    for(int i = 0; i < newNum.length - 1; i++){
+	        for(int j = i + 1; j < newNum.length; j++){
+	            if(newNum[i] > newNum[j]){
+	                double temp = newNum[i];
+	                newNum[i] = newNum[j];
+	                newNum[j] = temp;
+	            }
+	        }
+	    }
+	    
+	    if(Arrays.equals(newNum, num)){
+	        isSorted = true;
+	    }
+	       
+	    return isSorted;
 	}
 
 }
