004ecf5eef4714a7db5f6f68b75cf615daa7660c
AggieWB <usmaggie23@gmail.com>
2020-02-25 11:38:58 -0800

    Add necessary asterisks in comment block

--- itc115/itc115-assignment8/src/IsSorted.java
+++ itc115/itc115-assignment8/src/IsSorted.java
@@ -2,13 +2,13 @@
 import java.util.*;
 
 /* 
- This class contains a static method named isSorted that accepts an array of doubles as a parameter and returns true if the list is in sorted 
- (nondecreasing) order and false otherwise. For example, if arrays named list1 and list2 store {16.1, 12.3, 22.2, 14.4} and 
- {1.5, 4.3, 7.0, 19.5, 25.1, 46.2} respectively, the calls isSorted(list1) and isSorted(list2) should return false and true respectively. 
- A one-element array is considered to be sorted.  
- 
- Aggie Wheeler Bateman
- 2/25/20 
+ *This class contains a static method named isSorted that accepts an array of doubles as a parameter and returns true if the list is in sorted 
+ *(nondecreasing) order and false otherwise. For example, if arrays named list1 and list2 store {16.1, 12.3, 22.2, 14.4} and 
+ *{1.5, 4.3, 7.0, 19.5, 25.1, 46.2} respectively, the calls isSorted(list1) and isSorted(list2) should return false and true respectively. 
+ *A one-element array is considered to be sorted.  
+ *
+ *Aggie Wheeler Bateman
+ *2/25/20 
  */
 
 public class IsSorted {
