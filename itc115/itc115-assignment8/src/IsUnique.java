e8b43c920fa9c457d8305a87251ba03adfd93910
AggieWB <usmaggie23@gmail.com>
2020-02-25 11:40:42 -0800

    Add description of class and its methods, author, and date

--- itc115/itc115-assignment8/src/IsUnique.java
+++ itc115/itc115-assignment8/src/IsUnique.java
@@ -1,5 +1,12 @@
 import java.util.*;
 
+/*This class contains a method named isUnique that takes an array of integers as a parameter and that returns a boolean 
+ * value indicating whether or not the values in the array are unique (true for yes, false for no). 
+ * The values in the list are considered unique if there is no pair of values that are equal. 
+ * 
+ * Aggie Wheeler Bateman
+ * 2/25/20
+ */
 public class IsUnique {
 
 	public static void main(String[] args) {
