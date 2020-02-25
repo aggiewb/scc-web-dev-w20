bd45d0fe4b2cff4391eaf01801fb87c1d68a5b3f
AggieWB <usmaggie23@gmail.com>
2020-02-25 11:27:10 -0800

    Assign array variables and use arrays to test calls of isSorted method
    Add comments to show what the expected result will be

--- itc115/itc115-assignment8/src/IsSorted.java
+++ itc115/itc115-assignment8/src/IsSorted.java
@@ -2,9 +2,24 @@
 import java.util.*;
 
 public class IsSorted {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double[] arr1 = {16.1, 12.3, 22.2, 14.4}; 
+		System.out.println(isSorted(arr1));
+		//returns false
+		
+		double[] arr2 = {42};
+		System.out.println(isSorted(arr2));
+		//returns true
+		
+		double[] arr3 = {1.5, 4.3, 7.0, 19.5, 25.1, 46.2};
+		System.out.println(isSorted(arr3));
+		//returns true
+		
+		double[] arr4 = {42.0, 27.0};
+		System.out.println(isSorted(arr4));
+		//returns false
+		
 
 	}
 	
