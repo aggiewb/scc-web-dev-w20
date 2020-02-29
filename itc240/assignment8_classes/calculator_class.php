e33971014efbb8843bf6529ca0612df676d9aa72
AggieWB <usmaggie23@gmail.com>
2020-02-29 08:33:24 -0800

    Add 4 function definitions for each basic arithmetic operation

--- itc240/assignment8_classes/calculator_class.php
+++ itc240/assignment8_classes/calculator_class.php
@@ -3,6 +3,22 @@
 class Calculator {
     var $num1 = 0;
     var $num2 = 0;
+
+    function add($num1, $num2){
+
+    }
+
+    function substract($num1, $num2){
+        
+    }
+
+    function divide($num1, $num2){
+        
+    }
+
+    function multiply($num1, $num2){
+        
+    }
 }
 
 ?>
