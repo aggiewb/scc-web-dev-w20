b9123ec6e5cc02830f6dfb4cee0bea4acdfcfea9
AggieWB <usmaggie23@gmail.com>
2020-02-29 07:47:01 -0800

    Add php file for assignment, and add commented instructions

---
+++ itc240/assignment8_classes.php
@@ -0,0 +1,5 @@
+<?php 
+/* Assignment: Write a class to represent a basic artithmetic operations calculator. 
+Your class should have the ability to add, subtract, multiply or divide two numbers. 
+Be sure to add several tests for your calculator class. */
+?>
