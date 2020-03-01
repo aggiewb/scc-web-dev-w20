c473581f7cbc60b4febb150feec6c2a3dc75087e
AggieWB <usmaggie23@gmail.com>
2020-02-29 22:48:05 -0800

    Add calculator class

--- itc240/assignment8_classes/assignment8_classes.php
+++ itc240/assignment8_classes/assignment8_classes.php
@@ -1,5 +1,33 @@
-<?php 
+<?php
+
 /* Assignment: Write a class to represent a basic artithmetic operations calculator. 
 Your class should have the ability to add, subtract, multiply or divide two numbers. 
 Be sure to add several tests for your calculator class. */
+
+class Calculator {
+    public var $num1 = 0;
+    public var $num2 = 0;
+    public var $result = 0;
+
+    function add(){
+        $this->result = $this->num1 + $this->num2;
+        return $this->result; 
+    }
+
+    function substract(){
+        $this->result = $this->num1 - $this->num2;
+        return $this->result;   
+    }
+
+    function divide(){
+        $this->result = $this->num1 / $this->num2;
+        return $this->result;   
+    }
+
+    function multiply(){
+        $this->result = $this->num1 * $this->num2;
+        return $this->result; 
+    }
+}
+
 ?>
