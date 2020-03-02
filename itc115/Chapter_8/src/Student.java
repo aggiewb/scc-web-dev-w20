e6c152c6f8ed94516f7c25d73713136bcd9eda55
Aggie Wheeler Bateman <48076903+aggiewb@users.noreply.github.com>
2020-03-02 14:52:17 -0800

    Add attributes and constructors

--- itc115/Chapter_8/src/Student.java
+++ itc115/Chapter_8/src/Student.java
@@ -1,4 +1,24 @@
 
 public class Student {
+	private String firstName;
+	private String lastName;
+	private int studentID;
+	private String dateOfBirth;
+
+	
+	public Student(String lastName, String firstName, int studentID) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.studentID = studentID;
+	}
+	
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	
 
 }
