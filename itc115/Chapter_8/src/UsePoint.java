14dc9291551b67b69432d123b886c0b2a5d2a8e1
Aggie Wheeler Bateman <48076903+aggiewb@users.noreply.github.com>
2020-03-02 14:32:53 -0800

    Add class create new instances and call Point methods

--- itc115/Chapter_8/src/UsePoint.java
+++ itc115/Chapter_8/src/UsePoint.java
@@ -1,8 +1,18 @@
 
 public class UsePoint {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//create an instance of a Point object
+		Point p = new Point(2, 3);
+		
+		//get distance from origin (0,0)
+		double d = p.distanceFromOrigin();
+		System.out.println("Distance: " + d);
+		
+		//change x and y coordinates by 3 and 5
+		p.translate(3, 5);
+		
+		//demonstrate static method within a class
+		Point.hiPoint();
 
 	}
 
