f233d1144172e6ddabd0a0ac187086b530babdc9
Aggie Wheeler Bateman <48076903+aggiewb@users.noreply.github.com>
2020-03-02 13:40:20 -0800

    Add class attributes and methods

--- itc115/Chapter_8/src/Point.java
+++ itc115/Chapter_8/src/Point.java
@@ -1,5 +1,24 @@
-
 //Point object to represent a pair of coordinates
 public class Point {
+	//fields(attributes)
+	int x;
+	int y;
+	
+	//constructor to construct a new point with the given x and y location
+	public Point (int initialX, int initialY) {
+		x = initialX;
+		y = initialY;
+	}
+	
+	//return the distance to point of origin(0,0)
+	public double distanceFromOrigin() {
+		return Math.sqrt(x * x + y * y);
+	}
+	
+	//shift point to new coordinate
+	public void translate(int dX, int dY) {
+		x += dX;
+		y += dY;
+	}	
 
 }
