af048f27b9f193ff8d97a62033dd4730e150d8a2
AggieWB <usmaggie23@gmail.com>
2020-03-03 12:55:34 -0800

    Move input for loop to its own method
    Move output for loop to its own method
    Change variables to reflect parameters being passed in

--- itc115/itc115-assignment9/src/ProductClient.java
+++ itc115/itc115-assignment9/src/ProductClient.java
@@ -6,20 +6,27 @@ public class ProductClient {
 		Scanner input = new Scanner(System.in);
 		Product[] newProducts = new Product[3];
 		
+		userInput(input, newProducts);
+		userOutput(newProducts);
+		input.close();
+	}
+	
+	public static void userInput(Scanner console, Product[] newProducts) {
 		for(int i = 0; i < 3; i++) {
 			System.out.print("Please enter your product name and price: ");
-			String productName = input.next();
-			double price = input.nextDouble();
+			String productName = console.next();
+			double price = console.nextDouble();
 			newProducts[i] = new Product(productName, price);
 			System.out.print("Enter the product description: ");
-			String description = input.next();
+			String description = console.next();
 			newProducts[i].setDescription(description); 
-		}
-		
+		}	
+	}
+	
+	public static void userOutput(Product[] newProducts) {
 		for(int j = 0; j < 3; j++) {
 			System.out.println("Product number " + (j + 1) + " is " + newProducts[j]);
 		}
-		
-		input.close();
 	}
+	
 }
