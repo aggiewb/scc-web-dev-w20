ec4345b74231bacd7d182ea186601a211312f116
AggieWB <usmaggie23@gmail.com>
2020-03-03 12:19:50 -0800

    Add scanner, variables to store input, and print of Product

--- itc115/itc115-assignment9/src/ProductClient.java
+++ itc115/itc115-assignment9/src/ProductClient.java
@@ -1,11 +1,21 @@
 //create instances of 2-3 different products
 //use toString to display their data
-
+import java.util.*;
 public class ProductClient {
-
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter your product name and price: ");
+		String productName = input.next();
+		double price = input.nextInt();
 		
-
+		while(productName != " ") {
+			Product newProduct = new Product(productName, price);
+			System.out.println("Product is " + newProduct);
+			newProduct.getCount();
+			productName = input.next();
+			price = input.nextInt();
+		}
+		
+		input.close();
 	}
-
 }
