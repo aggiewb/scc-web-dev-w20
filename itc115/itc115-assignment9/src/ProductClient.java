43f257792d02a04fbb83c8950d6fc254da95be4d
AggieWB <usmaggie23@gmail.com>
2020-03-03 12:47:14 -0800

    Add array to store product objects
    Add for loop to get info from user, create object, and store in array
    Add for loop to print each object back to user

--- itc115/itc115-assignment9/src/ProductClient.java
+++ itc115/itc115-assignment9/src/ProductClient.java
@@ -4,16 +4,20 @@ import java.util.*;
 public class ProductClient {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Please enter your product name and price: ");
-		String productName = input.next();
-		double price = input.nextInt();
+		Product[] newProducts = new Product[3];
 		
-		while(productName != " ") {
-			Product newProduct = new Product(productName, price);
-			System.out.println("Product is " + newProduct);
-			newProduct.getCount();
-			productName = input.next();
-			price = input.nextInt();
+		for(int i = 0; i < 3; i++) {
+			System.out.print("Please enter your product name and price: ");
+			String productName = input.next();
+			double price = input.nextDouble();
+			newProducts[i] = new Product(productName, price);
+			System.out.print("Enter the product description: ");
+			String description = input.next();
+			newProducts[i].setDescription(description); 
+		}
+		
+		for(int j = 0; j < 3; j++) {
+			System.out.println("Product number " + (j + 1) + " is " + newProducts[j]);
 		}
 		
 		input.close();
