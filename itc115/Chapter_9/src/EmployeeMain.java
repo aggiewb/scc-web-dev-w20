320ec8573b0007b48720b2d3ae6cd03d02b2ad12
AggieWB <usmaggie23@gmail.com>
2020-03-09 14:05:07 -0700

    Add array to contain new instances and a for loop to use class methods on each instance

--- itc115/Chapter_9/src/EmployeeMain.java
+++ itc115/Chapter_9/src/EmployeeMain.java
@@ -2,8 +2,12 @@
 public class EmployeeMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Employee[] e = { new Lawyer(), new Secretary(), new Marketer(), new LegalSecretary() };
+		
+		for (int i = 0; i < e.length; i++) {
+			e[i].showSalary();
+			e[i].showVacation();
+			System.out.println();
+		}
 	}
-
 }
