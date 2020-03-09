7b8c2ac67f01e454f6fd1ed301685b3a17abb5a1
AggieWB <usmaggie23@gmail.com>
2020-03-09 13:55:31 -0700

    Add extension of Employee superclass, override method, and add advertise() method

--- itc115/Chapter_9/src/Marketer.java
+++ itc115/Chapter_9/src/Marketer.java
@@ -1,4 +1,13 @@
 
-public class Marketer {
-
+public class Marketer extends Employee {
+    public void showSalary() {
+        System.out.println("My salary is $50,000");
+    }
+    
+    public void advertise() {
+        System.out.println("Act now, while supplies last!");
+    }
+    
 }
+
+
