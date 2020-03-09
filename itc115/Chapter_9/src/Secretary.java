e627c145955b8ff8635257efa044a391a0b29846
AggieWB <usmaggie23@gmail.com>
2020-03-09 13:57:32 -0700

    Add extension of Employee superclass, override constructor, and add takeDictation() method

--- itc115/Chapter_9/src/Secretary.java
+++ itc115/Chapter_9/src/Secretary.java
@@ -1,4 +1,10 @@
 
-public class Secretary {
-
+public class Secretary extends Employee {
+    public Secretary() {
+        super();
+    }
+    
+    public void takeDictation(String text) {
+        System.out.println("Taking dictation of text: " + text);
+    }
 }
