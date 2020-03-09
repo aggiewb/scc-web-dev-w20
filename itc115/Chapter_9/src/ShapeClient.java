5f7dd3e3164bef5738a0e74ecf55f3178b9b8f3f
AggieWB <usmaggie23@gmail.com>
2020-03-09 15:00:58 -0700

    Add circle class and new instance of circle to client with method calls

--- itc115/Chapter_9/src/ShapeClient.java
+++ itc115/Chapter_9/src/ShapeClient.java
@@ -2,7 +2,23 @@
 public class ShapeClient {
 
 	public static void main(String[] args) {
-		double width = 7, height = 5, perimeter, area;
+		double radius = 7, perimeter, area;
+		
+		Circle circle = new Circle(radius);
+		area = circle.calculateArea();
+		System.out.println(area);
+		
+		perimeter = circle.calculatePerimeter();
+		//System.out.println(circle.toString());
+		System.out.println(perimeter);
+		
+		circle.setRadius(9);
+		
+		perimeter = circle.calculatePerimeter();
+		//System.out.println(circle.toString());
+		System.out.println(perimeter);
+		
+		double width = 7, height = 5;
 		
 		Rectangle rect1 = new Rectangle(width, height);
 		area = rect1.calculateArea();
