a68e8e069c88950476361d207571c7253a4baadb
AggieWB <usmaggie23@gmail.com>
2020-03-09 13:27:30 -0700

    Add stock class

--- itc115/Chapter_8/src/Stock.java
+++ itc115/Chapter_8/src/Stock.java
@@ -1,35 +1,41 @@
-//An object to represent purchases of shares of stocks
+// A Stock object represents purchases of shares of a stock.
 public class Stock {
-	private String symbol;
-	private int totalShares;
-	private double totalCost;
-	
-	//initialize a new Stock without shares purchased
-	public Stock(String theSymbol) {
-		if(theSymbol == null) {
-			throw new NullPointerException();
-		}
-		symbol = theSymbol;
-		totalShares = 0;
-		totalCost = 0.0;
-	}
-	
-	//return total P&L by the stock based on a given price per share
-	public double getProfit(double currentPrice) {
-		if(currentPrice < 0) {
-			throw new IllegalArgumentException();
-		}
-		double marketValue = totalShares * currentPrice;
-		return marketValue - totalCost;
-	}
-	
-	public void purchase(int shares, double pricePerShare) {
-		if(share < 0 || pricePerShare < 0.0) {
-			throw new IllegalArgumentException();
-		}
-		
-		totalShares += shares;
-		totalCost += shares * pricePerShare;
-	}
+    private String symbol;        // stock symbol, e.g. "YHOO"
+    private int totalShares;      // total shares purchased
+    private double totalCost;     // total cost for all shares
 
+    // Initializes a new Stock with no shares purchased.
+    // Precondition: symbol != null
+    public Stock(String theSymbol) {
+        if (theSymbol == null) {
+            throw new NullPointerException();
+        }
+
+        symbol = theSymbol;
+        totalShares = 0;
+        totalCost = 0.0;
+    }
+
+    // Returns the total profit or loss earned on this stock,
+    // based on the given price per share.
+    // pre: currentPrice >= 0.0
+    public double getProfit(double currentPrice) {
+        if (currentPrice < 0.0) {
+            throw new IllegalArgumentException();
+        }
+
+        double marketValue = totalShares * currentPrice;
+        return marketValue - totalCost;
+    }
+
+    // Records purchase of the given shares at the given price.
+    // pre: shares >= 0 && pricePerShare >= 0.0
+    public void purchase(int shares, double pricePerShare) {
+        if (shares < 0 || pricePerShare < 0.0) {
+            throw new IllegalArgumentException();
+        }
+
+        totalShares += shares;
+        totalCost += shares * pricePerShare;
+    }
 }
