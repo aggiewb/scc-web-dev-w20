9c455880b46b141d954366025280354855f20bfe
AggieWB <usmaggie23@gmail.com>
2020-03-09 14:00:39 -0700

    Add extension of Secretary class, override methods, and add fileLegalBriefs() method

--- itc115/Chapter_9/src/LegalSecretary.java
+++ itc115/Chapter_9/src/LegalSecretary.java
@@ -1,4 +1,19 @@
 
-public class LegalSecretary {
+public class LegalSecretary extends Secretary{
 
+    public LegalSecretary() {
+        super();
+    }
+
+    public void showSalary() {
+        System.out.println("My salary is $45,000");
+        
+    }
+    
+    public void fileLegalBriefs() {
+        System.out.println("I can file all day!");
+    }
+    
+    
 }
+
