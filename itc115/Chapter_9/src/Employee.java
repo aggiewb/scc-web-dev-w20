b7768d95a39484e70fe7313e857a341715ec2c45
AggieWB <usmaggie23@gmail.com>
2020-03-09 13:51:47 -0700

    Add methods to superclass

--- itc115/Chapter_9/src/Employee.java
+++ itc115/Chapter_9/src/Employee.java
@@ -1,4 +1,19 @@
 
 public class Employee {
-
+	public void applyForVacation() {
+        System.out.println("Use the yellow form.");
+    }
+    
+    public void showHours() {
+        System.out.println("I work 40 hours per week.");
+        
+    }
+    
+    public void showSalary() {
+        System.out.println("My salary is $40,000");
+    }
+    
+    public void showVacation() {
+        System.out.println("I receive 2 weeks of vacation");
+    }
 }
