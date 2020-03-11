bb101f180fe8713a7f804d652e120a96443df546
AggieWB <usmaggie23@gmail.com>
2020-03-11 10:56:27 -0700

    Add three methods and calls to demonstrate creation of objects

--- itc115/itc115-assignment10/src/TicketClient.java
+++ itc115/itc115-assignment10/src/TicketClient.java
@@ -1,6 +1,36 @@
 
 public class TicketClient {
 	public static void main(String[] args) {
+		WalkupTicket();
+		System.out.println();
+		AdvanceTicket();
+		System.out.println();
+		StudentAdvanceTicket();
+	}
+	
+	public static void WalkupTicket() {
+		for(int i = 1; i <= 3; i++) {
+			WalkupTicket ticket = new WalkupTicket(i);
+			System.out.println(ticket);
+		}
+	}
+	
+	public static void AdvanceTicket() {
+		int days = 0;
+		for(int i = 1; i <= 5; i++) {
+			AdvanceTicket ticket = new AdvanceTicket(i, days);
+			System.out.println(ticket);
+			days+=5;
+		}
+	}
+	
+	public static void StudentAdvanceTicket() {
+		int days = 0;
+		for(int i = 1; i <= 5; i++) {
+			StudentAdvanceTicket ticket = new StudentAdvanceTicket(i, days);
+			System.out.println(ticket);
+			days+=5;
+		}
 	}
 
 }
