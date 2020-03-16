781b48e9212685f56be8f40b50fdc5c1dd89b3ff
AggieWB <usmaggie23@gmail.com>
2020-03-16 12:52:24 -0700

    Override showHours(), showSalary(), and showVacation()
    Add clean() method

--- itc115/itc115-assignment11/src/Janitor.java
+++ itc115/itc115-assignment11/src/Janitor.java
@@ -1,4 +1,18 @@
 
-public class Janitor {
+public class Janitor extends Employee {
+	    public void showHours() {
+	        System.out.println("I work 80 hours per week.");
+	    }
 
+	    public void showSalary() {
+	        System.out.println("My salary is $30,000.");
+	    }
+
+	    public void showVacation() {
+	        System.out.println("I receive 1 week of vacation.");
+	    }
+	    
+	    public void clean() {
+	    	System.out.println("Workin' for the man.");
+	    }
 }
