5666b7e2503061ff75ea0dfe2247c8e9e9c6cc8c
AggieWB <usmaggie23@gmail.com>
2020-03-16 13:22:07 -0700

    Override showVacation() and applyforVacation
    Extend Lawyer class

--- itc115/itc115-assignment11/src/HavardLawyer.java
+++ itc115/itc115-assignment11/src/HavardLawyer.java
@@ -1,4 +1,12 @@
 
-public class HavardLawyer {
-
+public class HavardLawyer extends Lawyer{
+	 public void applyForVacation() {
+	        for(int i = 0; i < 3; i++) {
+	        	super.applyForVacation();
+	        }
+	    }
+	    
+	    public void showVacation() {
+	        System.out.println("I receive 3 weeks and 3 days vacation.");
+	    }
 }
