17f331ef3cdea52ad7e48a78eb1058979894ee36
AggieWB <usmaggie23@gmail.com>
2020-03-16 13:24:18 -0700

    Override showSalary() method

--- itc115/itc115-assignment11/src/HavardLawyer.java
+++ itc115/itc115-assignment11/src/HavardLawyer.java
@@ -1,12 +1,16 @@
 
 public class HavardLawyer extends Lawyer{
-	 public void applyForVacation() {
-	        for(int i = 0; i < 3; i++) {
-	        	super.applyForVacation();
-	        }
-	    }
-	    
-	    public void showVacation() {
-	        System.out.println("I receive 3 weeks and 3 days vacation.");
-	    }
+	public void showSalary() {
+        System.out.println("My salary is $48,000.");
+    }
+	
+	public void applyForVacation() {
+        for(int i = 0; i < 3; i++) {
+        	super.applyForVacation();
+        }
+    }
+    
+    public void showVacation() {
+        System.out.println("I receive 3 weeks and 3 days vacation.");
+    }
 }
