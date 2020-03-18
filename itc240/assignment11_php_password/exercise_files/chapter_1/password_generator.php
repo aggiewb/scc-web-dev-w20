3aa60411b3d81bca83d95e73b22b58325d3029e1
AggieWB <usmaggie23@gmail.com>
2020-03-18 10:36:24 -0700

    Create a character set, and select a random character

---
+++ itc240/assignment11_php_password/exercise_files/chapter_1/password_generator.php
@@ -0,0 +1,27 @@
+<?php
+$lower= 'abcdefghijklmnopqrstuvwxyz';
+$upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$numbers = '0123456789';
+$symbols = '$*?!-';
+$chars= $lower . $upper .$numbers . $symbols;
+
+//can also use PHP range()
+$upper_array= range('A', 'Z');
+$num_array = range(0, 9);
+$lower_array = range('a', 'z');
+$lower = implode('', $lower_array);
+$upper = implode('', $upper_array);
+$num = implode('', $num_array);
+
+function random_char($string){
+    $i = mt_rand(0, strlen($string)-1);
+    return $string[$i];
+}
+
+?>
+<html>
+<p><pre><?php print_r($lower_array) ?><p></pre>
+<p><?php echo $chars ?><p>
+<p><?php echo $lower . $upper . $num . $symbols?><p>
+<p><?php echo random_char($chars); ?><p>
+</html>
