bea03b773080b25fdd8e03659bfa94d5f482247c
AggieWB <usmaggie23@gmail.com>
2020-03-18 11:12:10 -0700

    Add form element with content, and move p elements into body
    Add link elements for css and head element

--- itc240/assignment11_php_password/exercise_files/chapter_1/password_generator.php
+++ itc240/assignment11_php_password/exercise_files/chapter_1/password_generator.php
@@ -50,10 +50,31 @@ function generate_password($length){
 
 ?>
 <html>
-<p><pre><?php print_r($lower_array) ?><p></pre>
-<p><?php echo $chars ?><p>
-<p><?php echo $lower . $upper . $num . $symbols?><p>
-<p><?php echo random_char($chars); ?><p>
-<p><?php echo random_string(10, $chars); ?><p>
-<p><?php echo generate_password(8); ?><p>
-</html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Password Generator</title>
+        <link rel="stylesheet" type="text/css" href="../../../basic_style.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans|Poppins&display=swap" rel="stylesheet"> 
+    </head>
+    <body>
+        <p><pre><?php print_r($lower_array) ?><p></pre>
+        <p><?php echo $chars ?><p>
+        <p><?php echo $lower . $upper . $num . $symbols?><p>
+        <p><?php echo random_char($chars); ?><p>
+        <p><?php echo random_string(10, $chars); ?><p>
+        <p><?php echo generate_password(8); ?><p>
+
+        <p>Generated Password: <?php echo $password; ?></p>
+        
+        <p>Generate a new password using the form options.</p>
+        <form action="" method="get">
+            Length: <input type="text" name="length" value="<?php if(isset($_GET['length'])) { echo $_GET['length']; } ?>" /><br />
+            <input type="checkbox" name="lower" value="1" <?php if($_GET['lower'] == 1) { echo 'checked'; } ?> /> Lowercase<br />
+            <input type="checkbox" name="upper" value="1" <?php if($_GET['upper'] == 1) { echo 'checked'; } ?> /> Uppercase<br />
+            <input type="checkbox" name="numbers" value="1" <?php if($_GET['numbers'] == 1) { echo 'checked'; } ?> /> Numbers<br />
+            <input type="checkbox" name="symbols" value="1" <?php if($_GET['symbols'] == 1) { echo 'checked'; } ?> /> Symbols<br />
+            <input type="submit" value="Submit" />
+        </form>
+
+    </body>
+  </html>
