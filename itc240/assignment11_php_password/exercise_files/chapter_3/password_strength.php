e41b37ce6b119e3575d8498298f378b740cc5231
AggieWB <usmaggie23@gmail.com>
2020-03-18 16:30:03 -0700

    Add functions to count numbers and symbols
    Increase possible points
    Call count numbers and symbols functions in password strength function to add points to strength count

--- itc240/assignment11_php_password/exercise_files/chapter_3/password_strength.php
+++ itc240/assignment11_php_password/exercise_files/chapter_3/password_strength.php
@@ -10,9 +10,22 @@ function detect_any_lowercase($string){
     return strtoupper($string) != $string;
 }
 
+function count_numbers($string){
+    return preg_match_all('/[0-9]/', $string);
+}
+
+function count_symbols($string){
+    //You have to decide which symbols count
+    //Regex \W is any non-letter, non-number: too broad
+    //Better to list the ones that count
+    //Escape regex symbols to get their literal values
+    $regex = '/[' . preg_quote('!@#$%^&*-_+=?') . ']/';
+    return preg_match_all($regex, $string);
+}
+
 function password_strength($password){
     $strength = 0;
-    $possible_points = 2;
+    $possible_points = 6;
 
     if(detect_any_uppercase($password)){
         $strength += 1;
@@ -21,6 +34,9 @@ function password_strength($password){
     if(detect_any_lowercase($password)){
         $strength += 1;
     }
+
+    $strength += min(count_numbers($password), 2);
+    $strength += min(count_symbols($password), 2);
 
     $strength_percent = $strength / (float)$possible_points;
     $rating = floor($strength_percent * 10);
